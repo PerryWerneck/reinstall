3cb9bab97b67839fb7f702613f180ed091c84f8a
Perry Werneck <perry.werneck@gmail.com>
2022-07-14 11:57:31 -0300

    Adding template for EFI based virtual machine.

--- src/library/run.sh
+++ src/library/run.sh
@@ -1,5 +1,67 @@
 #!/bin/bash
 
+VM_URI="qemu:///system"
+VM_NAME="reinstall"
+VM_TYPE="efi"
+
+if [ -e ./kvm/libvirt.conf ]; then
+	. ./kvm/libvirt.conf
+fi
+
+isolinux() {
+
+	virsh \
+		-c "${VM_URI}" \
+		undefine \
+		"${VM_NAME}"
+
+	sudo qemu-img \
+		create \
+		-f qcow2 \
+		/tmp/reinstall.qcow2 \
+		20G
+	if [ "$?" != "0" ]; then
+		exit -1
+	fi
+	
+	virsh \
+		-c "${VM_URI}" \
+		define \
+		kvm/isolinux-usb.xml
+	if [ "$?" != "0" ]; then
+		exit -1
+	fi
+
+}
+
+efi() {
+
+	virsh \
+		-c "${VM_URI}" \
+		undefine \
+		"${VM_NAME}"
+
+	sudo qemu-img \
+		create \
+		-f qcow2 \
+		/tmp/reinstall.qcow2 \
+		20G
+	if [ "$?" != "0" ]; then
+		exit -1
+	fi
+
+	cp kvm/vars.fd /tmp/vars.fd
+	if [ "$?" != "0" ]; then
+		exit -1
+	fi
+
+	virsh \
+			-c "${VM_URI}" \
+			define \
+			kvm/efi.xml
+
+}
+
 make Debug
 if [ "$?" != "0" ]; then
 	exit -1
@@ -12,28 +74,7 @@ if [ "$?" != "0" ]; then
 	exit -1
 fi
 
-. ./kvm/libvirt.conf
+${VM_TYPE}
 
-virsh \
-	-c "${VM_URI}" \
-	undefine \
-	"${VM_NAME}"
-
-sudo qemu-img \
-	create \
-	-f qcow2 \
-	/tmp/reinstall.qcow2 \
-	20G
-if [ "$?" != "0" ]; then
-	exit -1
-fi
- 
-virsh \
-	-c "${VM_URI}" \
-	define \
-	kvm/isolinux-usb.xml
-if [ "$?" != "0" ]; then
-	exit -1
-fi
 
 
