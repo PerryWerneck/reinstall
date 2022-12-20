b547f4e92cfc7a45fcacbd677b53651e2fed7b60
Perry Werneck <perry.werneck@gmail.com>
2022-12-19 22:39:39 -0300

    Addin mainwindow.

--- src/gui/main.cc
+++ src/gui/main.cc
@@ -19,19 +19,75 @@
 
  #include <config.h>
  #include <gtkmm.h>
+ #include <glibmm/i18n.h>
+ #include <iostream>
+
+ using namespace Gtk;
+ using namespace std;
 
  int main(int argc, char* argv[]) {
 
-	class MainWindow : public Gtk::Window {
+	class MainWindow : public Window {
+	private:
+		Label title{ _( "Select option" ) };
+		Gtk::Box hbox, vbox;
+		Gtk::ScrolledWindow view;
+		Gtk::ButtonBox bbox;
+		Gtk::Button apply{_("Apply")};
+		Gtk::Button cancel{_("Cancel")};
+
 	public:
 		MainWindow() {
-		  set_title(PACKAGE_STRING);
-		  set_default_size(200, 200);
+
+			set_title(PACKAGE_STRING);
+			set_default_size(600, 400);
+
+			// https://developer-old.gnome.org/gtkmm-tutorial/stable/sec-clipboard-examples.html.en
+			// http://transit.iut2.upmf-grenoble.fr/doc/gtkmm-3.0/tutorial/html/sec-helloworld.html
+
+			vbox.set_hexpand(true);
+			vbox.set_vexpand(true);
+			vbox.set_orientation(ORIENTATION_VERTICAL);
+			vbox.set_border_width(6);
+
+			vbox.add(title);
+
+			view.set_hexpand(true);
+			view.set_vexpand(true);
+			vbox.add(view);
+
+			bbox.add(cancel);
+			bbox.add(apply);
+			bbox.set_layout(BUTTONBOX_END);
+			bbox.set_hexpand(true);
+			vbox.add(bbox);
+
+			/*
+			grid.set_hexpand(true);
+			grid.set_vexpand(true);
+			grid.set_row_homogeneous(false);
+			grid.set_column_homogeneous(false);
+
+			grid.attach(title,1,0,1,1);		// widget, left, top, width, height
+
+
+			bbox.add(cancel);
+			bbox.add(apply);
+			bbox.set_border_width(6);
+			bbox.set_hexpand(true);
+			grid.attach(bbox,1,2,1,1);
+
+			*/
+
+			hbox.set_hexpand(true);
+			hbox.set_vexpand(true);
+			hbox.add(vbox);
+			add(hbox);
+			show_all();
 		}
 	};
 
-
-	auto app = Gtk::Application::create("br.com.bb.reinstall");
+	auto app = Application::create("br.com.bb.reinstall");
 
 	MainWindow window;
 	window.set_default_size(200, 200);
