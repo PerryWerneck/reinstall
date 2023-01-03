66b1f9d92180efa617bda08117605fa923476448
Perry Werneck <perry.werneck@gmail.com>
2023-01-03 13:53:48 -0300

    Implementing grub module.

---
+++ src/modules/grub/init.cc
@@ -0,0 +1,54 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+/*
+ * Copyright (C) 2022 Perry Werneck <perry.werneck@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ #include <config.h>
+ #include <udjat/module.h>
+ #include <udjat/factory.h>
+ #include <udjat/tools/object.h>
+ #include <stdexcept>
+ #include <reinstall/userinterface.h>
+ #include <reinstall/group.h>
+ #include <udjat/tools/quark.h>
+ #include <udjat/tools/logger.h>
+ #include <udjat/tools/intl.h>
+
+ using namespace std;
+ using namespace Udjat;
+
+ Udjat::Module * udjat_module_init() {
+
+ 	static const Udjat::ModuleInfo moduleinfo { "Grub based system installation" };
+
+	class Module : public Udjat::Module, public Udjat::Factory {
+	public:
+		Module() : Udjat::Module("local-installer", moduleinfo), Udjat::Factory("local-installer",moduleinfo) {
+		}
+
+		bool push_back(const pugi::xml_node &node) override {
+
+
+			return true;
+		}
+
+	};
+
+	return new Module();
+
+ }
+
